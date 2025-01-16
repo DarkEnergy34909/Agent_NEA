1d27ba04a27c224f0c0fbc71b3675c2e3fff80a0
Sebastian Kember <81592324+DarkEnergy34909@users.noreply.github.com>
2025-01-16 13:43:56 +0000

    Changed path

--- Agent_NEA/Level.h
+++ Agent_NEA/Level.h
@@ -63,7 +63,7 @@ class Level {
 
 	private:
 		// File path
-		const std::string PATH = "C:/Users/Sebastian Kember/OneDrive - King Edward VI Grammar School/CS A-Level/NEA/Media/";
+		const std::string PATH = "C:/Users/S_kem/OneDrive - King Edward VI Grammar School/CS A-Level/NEA/Media/";
 
 		// The window the level is rendered to
 		SDL_Window* window;
